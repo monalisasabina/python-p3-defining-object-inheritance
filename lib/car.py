c6ca537e67f5fe23bf5556a78bb93902b06ca24b
Monalisa Onyango <+>
2024-09-02 19:38:00 +0300

    inheritance

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+print(Car.__bases__) #(<class 'vehicle.Vehicle'>,)
+print(int.__bases__) #(<class 'object'>,)
+print(Car.__class__) #<class 'type'>
+print(int.__class__) #<class 'type'>
